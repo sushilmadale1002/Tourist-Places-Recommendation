18b00b6f5018ce9277d49afed5bbb8b0a97b79c4
sushilmadale1002 <madalesushil002@gmail.com>
2025-04-13 15:51:06 +0530

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,18 +1,19 @@
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications import MobileNetV2  # Lighter than VGG16
 from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 
 class FeatureExtractor:
     def __init__(self):
-        base_model = VGG16(weights='imagenet')
-        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+        base_model = MobileNetV2(weights='imagenet', include_top=False, 
+                               pooling='avg', input_shape=(224, 224, 3))
+        self.model = Model(inputs=base_model.input, outputs=base_model.output)
+        self.model._make_predict_function()  # For thread safety
 
     def extract(self, img):
-        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
-        img = img.convert('RGB')  # Make sure img is color
-        x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
-        x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C)
-        x = preprocess_input(x)  # Subtracting avg values for each pixel
-        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
-        return feature / np.linalg.norm(feature)  # Normalize
+        img = img.resize((224, 224)).convert('RGB')
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = x / 127.5 - 1.0  # MobileNetV2 preprocessing
+        feature = self.model.predict(x, verbose=0)[0]
+        return feature / np.linalg.norm(feature)
